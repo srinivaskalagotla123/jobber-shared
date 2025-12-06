b6754c94e222b00893e5e0c7023bd62f612e8348
srinivaskalagotla123 <srineehu@gmail.com>
2025-12-06 17:22:55 +0530

    added files

---
+++ src/logger.ts
@@ -0,0 +1,31 @@
+import winston, { Logger } from 'winston';
+import { ElasticsearchTransport } from 'winston-elasticsearch';
+
+export const winstonLogger = (elasticsearchNode: string, name: string, level: string): Logger => {
+  const options = {
+    console: {
+      level,
+      handleExceptions: true,
+      json: false,
+      colorize: true
+    },
+    elasticsearch: {
+      level,
+      clientOpts: {
+        node: elasticsearchNode,
+        log: level,
+        maxRetries: 2,
+        requestTimeout: 10000,
+        sniffOnStart: false
+      },
+      apm: false
+    }
+  };
+  const esTransport: ElasticsearchTransport = new ElasticsearchTransport(options.elasticsearch);
+  const logger: Logger = winston.createLogger({
+    exitOnError: false,
+    defaultMeta: { service: name },
+    transports: [new winston.transports.Console(options.console), esTransport]
+  });
+  return logger;
+}
